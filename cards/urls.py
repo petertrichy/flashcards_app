fa483e10560b7c29d6a20552699eabced74f9de2
PETER ANNA RAJU P <petertrichy@gmail.com>
2022-07-25 18:09:23 +0530

    Models and Flashcard view
    Models created and migrated. Data entered through python shell. Template for Flashcard display created.

--- cards/urls.py
+++ cards/urls.py
@@ -1,12 +1,13 @@
 # cars/urls.py
 
 from django.urls import path
-from django.views.generic import TemplateView
+
+from . import views
 
 urlpatterns = [
     path(
         "",
-        TemplateView.as_view(template_name="cards/base.html"),
-        name="home"
+        views.CardListView.as_view(),
+        name="card-list"
     ),
 ]
