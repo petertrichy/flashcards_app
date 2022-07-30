3987e967f22c72a8f837e65dd2d1d290adc3369e
PETER ANNA RAJU P <petertrichy@gmail.com>
2022-07-30 18:42:09 +0530

    Step6.2 Pick a Random card one
    part 2 of Step6 Picking a Random Card for display is completed

--- cards/views.py
+++ cards/views.py
@@ -1,5 +1,7 @@
 # cards/views.py
 
+import random
+
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -30,5 +32,7 @@ class BoxView(CardListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["box_number"] = self.kwargs["box_num"]
+        if self.object_list:
+            context["check_card"] = random.choice(self.object_list)
         return context
 
